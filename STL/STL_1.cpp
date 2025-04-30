011a96100c1baffed7a53d50e4a673ef4e980486
Utsav Yadav <ukscihub123@gmail.com>
2025-05-01 00:17:41 +0530

    Added Data Structure Folder Structure and Code.

--- STL/STL_1.cpp
+++ STL/STL_1.cpp
@@ -557,7 +557,7 @@ int main() {
 	pq.push(1); 
 	pq.push(5); 
 	pq.push(2); 
-	pq.push(6); 
+	pq.push(6);  7                                                                                                                        
 
 	cout << pq.top() << endl; // prints 1 
 
